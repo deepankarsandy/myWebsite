29a9c3d005f784b0f743cd3dba8cfd6722d2414f
Deepankar Sandhibigraha <37024019+deepankarsandy@users.noreply.github.com>
2020-03-08 09:00:24 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React, { PureComponent } from 'react';
+import './App.css';
+
+class App extends PureComponent{
+  render(){
+    return (
+      <div className="App">
+        <h1> Hello, World! </h1>
+        <h2> This is Sandy! </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
