748cf1d2a7a852af589587396deeebddf7ee6887
Deepankar Sandhibigraha <deepankarsandy@gmail.com>
2021-06-06 21:34:13 +0530

    covid19: filter options added

--- js/pages/covid19.jsx
+++ js/pages/covid19.jsx
@@ -4,13 +4,40 @@ modification history
 01a,06jun2021,deepankar created
 */
 
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import Button from '../components/button';
 import COVID19 from '../helpers/covid19';
 import DateTime from '../helpers/datetime';
 import isEmpty from '../helpers/isEmpty';
 
-export default class Covid19 extends PureComponent {
+function filterVaccines(vaccines, filters){
+  let filtered = vaccines;
+
+  if (filters.has('18+') && filters.has('45+')){
+    filtered = filtered.filter((v) => (v.min_age_limit === 18 || v.min_age_limit === 45));
+  } else if (filters.has('45+')){
+    filtered = filtered.filter((v) => v.min_age_limit === 45);
+  } else if (filters.has('18+')){
+    filtered = filtered.filter((v) => v.min_age_limit === 18);
+  }
+
+  if (filters.has('dose1') && filters.has('dose2')){
+    filtered = filtered.filter((v) => (
+      (v.available_capacity_dose1 > 0) || (v.available_capacity_dose2 > 0)
+    ));
+  } else if (filters.has('dose1')){
+    filtered = filtered.filter((v) => v.available_capacity_dose1 > 0);
+  } else if (filters.has('dose2')){
+    filtered = filtered.filter((v) => v.available_capacity_dose2 > 0);
+  }
+
+  if (filters.has('free')){
+    filtered = filtered.filter((v) => v.center.fee_type === 'Free');
+  }
+
+  return filtered;
+}
+export default class Covid19 extends Component {
   constructor(props){
     super(props);
 
@@ -19,12 +46,19 @@ export default class Covid19 extends PureComponent {
       date:     DateTime.dayjs(new Date()).format('YYYY-MM-DD'),
       pin:      755001,
       finding:  false,
+      filters:  new Map(),
+      origData: [],
     };
 
     this.findVaccines = this.findVaccines.bind(this);
     this.renderVaccineSessions = this.renderVaccineSessions.bind(this);
     this.onSetPin = this.onSetPin.bind(this);
     this.onSetDate = this.onSetDate.bind(this);
+    this.toggleFilters = this.toggleFilters.bind(this);
+  }
+
+  componentDidMount(){
+    this.findVaccines();
   }
 
   onSetDate({ target: { value } }){
@@ -37,17 +71,31 @@ export default class Covid19 extends PureComponent {
 
   findVaccines(){
     const {
-      pin, date, age, dose
+      pin, date, age, dose, filters
     } = this.state;
 
     this.setState({ finding: true }, () => {
       COVID19.vaccineAvailability(pin, date, age, dose)
         .then((vaccines) => {
-          this.setState({ vaccines });
+          this.setState({
+            vaccines: filterVaccines(vaccines, filters), origData: vaccines
+          });
         })
         .catch((error) => this.setState({ error }))
         .finally(() => this.setState({ finding: false }));
     });
+  }
+
+  toggleFilters({ currentTarget: { dataset: { filter } } }){
+    const { filters, origData } = this.state;
+
+    if (filters.has(filter)){
+      filters.delete(filter);
+    } else {
+      filters.set(filter);
+    }
+
+    this.setState({ filters, vaccines: filterVaccines(origData, filters) });
   }
 
   renderVaccineSessions(){
@@ -62,56 +110,54 @@ export default class Covid19 extends PureComponent {
     }
 
     return (
-      <table className="table is-striped is-hoverable is-fullwidth">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Place</th>
-            <th>Total Dose</th>
-            <th className="is-hidden-mobile">Dose 1</th>
-            <th className="is-hidden-mobile">Dose 2</th>
-            <th className="is-hidden-mobile">Address</th>
-            <th>Register</th>
-          </tr>
-        </thead>
-        <tbody>
-          {vaccines.map((v) => (
-            <tr key={v.session_id}>
-              <td>{/* DateTime.dayjs(v.date, 'DD-MM-YYYY').format('ddd, MMM DD') */ v.date}</td>
-              <td>{v.center.name}</td>
-              <td className="align-right">{v.available_capacity}</td>
-              <td className="is-hidden-mobile align-right">{v.available_capacity_dose1}</td>
-              <td className="is-hidden-mobile align-right">{v.available_capacity_dose2}</td>
-              <td className="is-hidden-mobile">
-                {v.center.address}
-                ,&nbsp;
-                {v.center.block_name}
-                ,&nbsp;
-                {v.center.pincode}
-              </td>
-              <td>
-                <a className="button is-link" href="https://selfregistration.cowin.gov.in/" target="cowin_register" title="register">
-                  Register
-                </a>
-              </td>
+      <div className="table-container">
+        <table className="table is-striped is-hoverable is-fullwidth">
+          <thead>
+            <tr>
+              <th className="no-wrap">Date</th>
+              <th colSpan={2}>Place</th>
+              <th className="no-wrap">Dose</th>
+              <th colSpan={3} className="is-hidden-mobile">Address</th>
+              <th>Register</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {vaccines.map((v) => (
+              <tr key={v.session_id}>
+                <td className="no-wrap">{/* DateTime.dayjs(v.date, 'DD-MM-YYYY').format('ddd, MMM DD') */ v.date}</td>
+                <td colSpan={2}>{v.center.name}</td>
+                <td className="align-right">{v.available_capacity}</td>
+                <td colSpan={3} className="is-hidden-mobile">
+                  {v.center.address}
+                  ,&nbsp;
+                  {v.center.block_name}
+                </td>
+                <td>
+                  <a className="button is-link is-size-7-mobile" href="https://selfregistration.cowin.gov.in/" target="cowin_register" title="register">
+                    Register
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 
   render(){
-    const { date, pin, finding } = this.state;
+    const {
+      date, pin, finding, filters
+    } = this.state;
 
     return (
-      <main className="page page--covid19 dark dark-theme">
+      <main className="page page--covid19 dark dark-theme is-size-7-mobile">
         <section className="hero is-dark is-bold is-fullheight-with-navbar root">
           <header className="hero-head">
             <div className="vaccine-finder">
               <div className="pincode field">
                 <div className="control">
-                  <input placeholder="Pincode" className="input" type="number" value={pin} min={111111} max={999999} onChange={this.onSetPin} />
+                  <input placeholder="Pincode" className="input" type="number" value={pin} onChange={this.onSetPin} />
                 </div>
               </div>
               <div className="field">
@@ -120,11 +166,56 @@ export default class Covid19 extends PureComponent {
                 </div>
               </div>
               <div className="control">
-                <Button label="Find Vaccines" className="is-link" onClick={this.findVaccines} isBusy={finding} />
+                <Button label="Find Vaccines" icon="fas fa-search" className="is-link is-size-7-mobile" onClick={this.findVaccines} isBusy={finding} />
+              </div>
+              <div className="control">
+                <Button label="Refresh" icon="fas fa-sync-alt" className="is-link is-size-7-mobile" onClick={this.findVaccines} isBusy={finding} />
               </div>
             </div>
           </header>
           <main className="hero-body">
+            <section className="filters buttons">
+              <Button
+                label="18+"
+                data-filter="18+"
+                onClick={this.toggleFilters}
+                className={`is-size-7-mobile ${filters.has('18+') ? 'is-link' : 'is-dark'}`}
+                iconAlignRight
+                icon={filters.has('18+') ? 'fas fa-check' : ''}
+              />
+              <Button
+                label="45+"
+                data-filter="45+"
+                onClick={this.toggleFilters}
+                className={`is-size-7-mobile ${filters.has('45+') ? 'is-link' : 'is-dark'}`}
+                iconAlignRight
+                icon={filters.has('45+') ? 'fas fa-check' : ''}
+              />
+              <Button
+                label="Dose 1"
+                data-filter="dose1"
+                onClick={this.toggleFilters}
+                className={`is-size-7-mobile ${filters.has('dose1') ? 'is-link' : 'is-dark'}`}
+                iconAlignRight
+                icon={filters.has('dose1') ? 'fas fa-check' : ''}
+              />
+              <Button
+                label="Dose 2"
+                data-filter="dose2"
+                onClick={this.toggleFilters}
+                className={`is-size-7-mobile ${filters.has('dose2') ? 'is-link' : 'is-dark'}`}
+                iconAlignRight
+                icon={filters.has('dose2') ? 'fas fa-check' : ''}
+              />
+              <Button
+                label="Free"
+                data-filter="free"
+                onClick={this.toggleFilters}
+                className={`is-size-7-mobile ${filters.has('free') ? 'is-link' : 'is-dark'}`}
+                iconAlignRight
+                icon={filters.has('free') ? 'fas fa-check' : ''}
+              />
+            </section>
             {this.renderVaccineSessions()}
           </main>
         </section>
